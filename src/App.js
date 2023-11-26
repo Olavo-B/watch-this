7108d49f0d1a4396c76b6ee4d4446ab280b1069b
Olavo-B <olavo.barros@ufv.br>
2023-11-26 19:44:58 -0300

    Refactor UI components and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
+import { FiSearch, FiShuffle} from 'react-icons/fi'
+
 
 function App() {
   return (
 
     <div className="container">
-      <div className="image"></div>
+      <div className="logo"></div>
 
-    <div className="containerInput">
-      <input 
-      type="text" 
-      placeholder="Search" />
+      <div className="containerInput">
+        <input 
+        type="text" 
+        placeholder="Search" />
 
-      <button className="buttonSearch">Search</button>
-    </div>
+        <button className="buttonSearch">
+          <FiSearch size={20} color="#FFF"/>
+        </button>
+
+        <button className="buttonShuffle">
+          <FiShuffle size={20} color="#FFF"/>
+        </button>
+      </div>
+
+      <main className="main">
+          <div className="card"></div>
+      </main>
 
     </div>
 
