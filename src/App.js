2c334cc6e58192e9cd47de7ca1c80c210b40b244
Olavo-B <olavo.barros@ufv.br>
2023-11-26 20:28:28 -0300

    Add search functionality and footer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import { FiSearch, FiShuffle} from 'react-icons/fi'
+import { FiSearch, FiShuffle, FiHeart} from 'react-icons/fi'
+import SearchBar from './components/SearchBar';
 
 
 function App() {
+
+  const handleSearch = (searchTerm) => {
+    console.log(`Realizando pesquisa por: ${searchTerm}`);
+    alert(`Realizando pesquisa por: ${searchTerm}`);
+    // Lógica de pesquisa aqui...
+  };
+
   return (
 
-    <div className="container">
-      <div className="logo"></div>
+    <div className="App">
+      <div className="container">
+        <div className="logo"></div>
 
-      <div className="containerInput">
-        <input 
-        type="text" 
-        placeholder="Search" />
+        {/* <div className="containerInput">
+          <input 
+          type="text" 
+          placeholder="Search" />
 
-        <button className="buttonSearch">
-          <FiSearch size={20} color="#FFF"/>
-        </button>
+          <button className="buttonSearch">
+            <FiSearch size={20} color="#FFF"/>
+          </button>
 
-        <button className="buttonShuffle">
-          <FiShuffle size={20} color="#FFF"/>
-        </button>
+          <button className="buttonShuffle">
+            <FiShuffle size={20} color="#FFF"/>
+          </button>
+        </div>
+
+        <main className="main">
+            <div className="card"></div>
+        </main> */}
+
+        <SearchBar onSearch={handleSearch}/>
+
       </div>
 
-      <main className="main">
-          <div className="card"></div>
-      </main>
-
+      <div className="footer">
+        <p>Created with <FiHeart size={10} color='black'/> by <a href="https://github.com/Olavo-B"
+        target="_blank">Olavo-B </a></p>
+      </div>
     </div>
-
-
   );
 }
 
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { FiSearch, FiShuffle } from 'react-icons/fi';
+import './style/SearchBar.css';
+
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handlers = {
+    submit1: handleSubmitSearch,
+    submit2: handleSubmitShuffle,
+  }
+  
+  const submitHandler = (e) => {
+    const { id } = e.nativeEvent.submitter; // <-- access submitter id
+    handlers[id](e); // <--proxy event to proper callback handler
+  };
+
+  function handleSubmitSearch(event) {
+    event.preventDefault();
+    onSearch(searchTerm);
+    console.log("Search");
+    event.target.reset();
+  }
+  
+  function handleSubmitShuffle(event) {
+    event.preventDefault();
+    alert("Shuffle");
+    event.target.reset();
+  };
+
+    return (
+
+        <form className="containerInput" onSubmit={submitHandler}>
+            <input 
+            type="text" 
+            placeholder="Search"
+            value={searchTerm}
+            onChange={handleChange} />
+
+            <button className="buttonSearch" id='submit1'>
+                <FiSearch size={20} color="#FFF"/>
+            </button>
+
+            <button className="buttonShuffle" id='submit2'>
+                <FiShuffle size={20} color="#FFF"/>
+            </button>
+        </form>
+
+        
+    );
+};
+
+export default SearchBar;
